ff70a20cb923852d4a098fc592a5d472cc06faf1
artemis <artemisxen@gmail.com>
2017-08-16 14:39:43 +0100

    added note-controller.js

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ var path = require("path");
 var pathToHtmlAndJsFiles = path.join(__dirname);
 var server = httpServer.createServer({ root: pathToHtmlAndJsFiles });
 server.listen(3000);
+console.log("Server is on");
--- note-controller.js
+++ note-controller.js
@@ -1,1 +1,19 @@
-document.getElementById("app").innerHTML = "Howdy"
+(function(exports) {
+  function NoteController(list) {
+    this.list = list;
+    list.createNote("buy bread");
+    this.view = new NoteListView(list);
+  }
+
+  NoteController.prototype.render = function () {
+    document.getElementById("app").innerHTML = this.view.renderList();
+    console.log(this.view.renderList());
+  };
+
+  exports.NoteController = NoteController;
+})(this);
+
+list = new List();
+controller = new NoteController(list);
+// controller.render();
+try { controller.render(); } catch(err) {}
